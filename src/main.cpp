6608a4666f447be289f925db807ed7e46d1579fc
Adam Butt <adamskibutt@gmail.com>
2022-05-04 23:52:21 +0100

    Test servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <Arduino.h>
 #include <IRremote.h>
+#include <Servo.h>
+
+Servo myservo;
+
+int potpin = 0;
+int val;
+
+int vol;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Hello");
   IrReceiver.begin(11, ENABLE_LED_FEEDBACK);
+
+  myservo.attach(9);
+
+  vol = myservo.read();
 }
 
 void loop() {
@@ -14,4 +26,9 @@ void loop() {
     Serial.println(IrReceiver.decodedIRData.command);
     IrReceiver.resume(); // Enable receiving of the next value
   }
+
+  val = analogRead(potpin);
+  val = map(val, 0, 1023, 0, 180);
+  myservo.write(val);
+  delay(15);
 }
